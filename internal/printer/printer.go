961aa9b5fa9785046abb60c3fa72e7bdef16bf99
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2026-03-25 16:18:37 -0600

    Refactor Printer struct (add fields and methods)

--- internal/printer/printer.go
+++ internal/printer/printer.go
@@ -1,11 +1,44 @@
 package printer
 
-import "io"
+import (
+	"fmt"
+	"io"
+)
 
 type Printer struct {
-	Out io.Writer
+	Stdout  io.Writer
+	Stderr  io.Writer
+	Verbose bool
 }
 
-func NewPrinter(out io.Writer) *Printer {
-	return &Printer{Out: out}
+func NewPrinter(out, err io.Writer) *Printer {
+	return &Printer{Stdout: out, Stderr: err}
+}
+
+func (p *Printer) Printf(format string, args ...any) {
+	_, _ = fmt.Fprintf(p.Stdout, format, args...)
+}
+
+func (p *Printer) Println(args ...any) {
+	_, _ = fmt.Fprintln(p.Stdout, args...)
+}
+
+func (p *Printer) Errorf(format string, args ...any) {
+	_, _ = fmt.Fprintf(p.Stderr, format, args...)
+}
+
+func (p *Printer) Errorln(args ...any) {
+	_, _ = fmt.Fprintln(p.Stderr, args...)
+}
+
+func (p *Printer) Verbosef(format string, args ...any) {
+	if p.Verbose {
+		_, _ = fmt.Fprintf(p.Stderr, format, args...)
+	}
+}
+
+func (p *Printer) Verboseln(args ...any) {
+	if p.Verbose {
+		_, _ = fmt.Fprintln(p.Stderr, args...)
+	}
 }
