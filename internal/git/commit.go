01996a68942a599d78eacdd755a063dcadc2e11f
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2026-03-30 17:04:03 -0600

    Create commit method for git client

---
+++ internal/git/commit.go
@@ -0,0 +1,16 @@
+package git
+
+func (g *GitCLIClient) Commit(repoDir string, args ...string) (string, error) {
+	commitArgs := []string{"commit"}
+	for _, msg := range args {
+		commitArgs = append(commitArgs, "--message")
+		commitArgs = append(commitArgs, msg)
+	}
+
+	commitOutput, err := g.Runner.Run(repoDir, "git", commitArgs...)
+	if err != nil {
+		return string(commitOutput), err
+	}
+
+	return string(commitOutput), nil
+}
