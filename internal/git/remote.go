bb82e3df832181b7827090dac7e992a06bbfd5fc
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2026-04-10 14:08:25 -0600

    Create git client RemoteAdd method

--- internal/git/remote.go
+++ internal/git/remote.go
@@ -10,3 +10,14 @@ func (g *GitCLIClient) Remote(orcaProfileDir string, args ...string) (string, er
 
 	return string(output), nil
 }
+
+func (g *GitCLIClient) RemoteAdd(orcaProfileDir string, args ...string) (string, error) {
+	args = append([]string{"remote", "add"}, args...)
+
+	output, err := g.Runner.Run(orcaProfileDir, "git", args...)
+	if err != nil {
+		return "", err
+	}
+
+	return string(output), nil
+}
