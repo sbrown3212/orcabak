50bc790e1da1e3d53a7345719c9e9c078b82a9f9
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2026-04-14 12:36:54 -0600

    Create PushSetUpstream method for git client

--- internal/git/push.go
+++ internal/git/push.go
@@ -8,3 +8,12 @@ func (g *GitCLIClient) Push(orcaConfigDir string) (string, error) {
 
 	return string(output), nil
 }
+
+func (g *GitCLIClient) PushSetUpstream(orcaConfigDir, remote, branch string) (string, error) {
+	output, err := g.Runner.Run(orcaConfigDir, "git", "push", "-u", remote, branch)
+	if err != nil {
+		return string(output), err
+	}
+
+	return string(output), nil
+}
